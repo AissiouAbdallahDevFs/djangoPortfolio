95c5c6aadaf68b92df2e087f8a21b1e893dc43dc
AissiouAbdallahDevFs <aissiou.abdallah@gmail.com>
2023-09-08 18:36:55 +0200

    firstTestUnit

--- myPortfolio/src/views.py
+++ myPortfolio/src/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets,status, permissions
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.decorators import login_required
 
 class IsSuperuserAndAdminUser(permissions.BasePermission):
@@ -12,10 +14,15 @@ class IsSuperuserAndAdminUser(permissions.BasePermission):
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()  
     serializer_class = UserSerializer  
 
 class GroupViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
