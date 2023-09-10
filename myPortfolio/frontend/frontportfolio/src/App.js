1fb9eefd3e3244e22b8cade1ca3f1ad6cb7ae6c7
AissiouAbdallahDevFs <aissiou.abdallah@gmail.com>
2023-09-10 14:45:35 +0200

    add front api react and ExperienceBubble component

---
+++ myPortfolio/frontend/frontportfolio/src/App.js
@@ -0,0 +1,13 @@
+import ExperienceBubble from './components/experiences/ExperienceBubble';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h2>Experiences</h2>
+        <ExperienceBubble />
+    </div>
+  );
+}
+
+export default App;
